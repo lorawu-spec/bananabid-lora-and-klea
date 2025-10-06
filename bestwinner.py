e80ed81e4b77035a2812c5bf49429da1d5e6a412
lorawu-spec <lorawu@hunterschools.org>
2025-10-06 18:28:23 -0400

    Update bestwinner.py

--- bestwinner.py
+++ bestwinner.py
@@ -1,11 +1,11 @@
 import random
 def bananabid(my_player_number, my_bananas, monkey_position, opponent_bananas, past_bid_list, turn_number):
 
-    if my_bananas >= 200: 
+    if my_bananas == 200: 
       return 51 
 
-    if my_bananas >= 150 
+    if my_bananas == 149: 
       return 61 
 
-    if my_bananas >= 88 
+    if my_bananas >= 88: 
       return 88
